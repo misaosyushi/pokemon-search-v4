de528faeb59223a15605fa9ccfd9b485d06a5b2a
misaosyushi <tujitamisaki@gmail.com>
2022-01-16 11:14:10 +0900

    add get href paths

--- app/app/spiders/scrapy_blog_spider.py
+++ app/app/spiders/scrapy_blog_spider.py
@@ -4,8 +4,10 @@ import scrapy
 class ScrapyBlogSpiderSpider(scrapy.Spider):
     name = 'scrapy_blog_spider'
     allowed_domains = ['pente.koro-pokemon.com']
-    start_urls = ['http://pente.koro-pokemon.com/zukan']
+    start_urls = ['http://pente.koro-pokemon.com/zukan/']
 
     def parse(self, response):
-        print(response.css('.ul2 .zukan_img'))
+        print('===================================')
+        # print(response.css('a[href*=zukan]::attr(href)').getall())
+        print(response.xpath('//a[contains(@href, "zukan") and contains(@href, "shtml")]/@href').getall())
         pass
