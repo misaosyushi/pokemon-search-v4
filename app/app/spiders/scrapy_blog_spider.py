f9fe3a9e4422ebe2ce8a92868746e6bba4766919
misaosyushi <tujitamisaki@gmail.com>
2022-01-30 11:17:30 +0900

    add todo

--- app/app/spiders/scrapy_blog_spider.py
+++ app/app/spiders/scrapy_blog_spider.py
@@ -9,5 +9,8 @@ class ScrapyBlogSpiderSpider(scrapy.Spider):
     def parse(self, response):
         print('===================================')
         # print(response.css('a[href*=zukan]::attr(href)').getall())
-        print(response.xpath('//a[contains(@href, "zukan") and contains(@href, "shtml")]/@href').getall())
+        # TODO: ulのzukan_imgを指定できたら良さそう
+        # ul指定して取ってからforで回す？
+        # print(response.xpath('//ul[@class="zukan_img"]/li/a[contains(@href, "zukan") and contains(@href, "shtml")]/@href').getall())
+        print(response.xpath('//ul[@class="ul2"]/a[contains(@href, "zukan") and contains(@href, "shtml")]/@href').getall())
         pass
